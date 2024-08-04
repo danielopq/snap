2c81360f1d42a6b0c5ad7c885bffe133be07d829
danielopq <danielopq@gmail.com>
2024-08-04 17:51:14 +0100

    snap.js - code cleaning and JSDoc added

--- js/snap.js
+++ js/snap.js
@@ -1,20 +1,35 @@
-window.onload = start;
+/**
+ * @fileoverview Contains the main actions to hide or display the main menu and the sub-menu while the mobile version is running.
+ * @author Daniel Martinez Duque
+ * @date 2024-08-04
+ */
 
-function start(){
+(()=>{
 	document.getElementById("mobile-close").addEventListener('click',closeMobileMenu,false);
 	document.getElementById("header-hamburger").addEventListener('click',showMobileMenu,false);
 	document.getElementById("featuresBt-mb").addEventListener('click',deploySubmenu,false);
 	document.getElementById("companyBt-mb").addEventListener('click',deploySubmenu,false);
-}
+})()
 
+/**
+ * Hide the menu.
+ */
 function closeMobileMenu(){
 	document.getElementById("mobile-nav").style.display ="none";
 }
 
+/**
+ * Display the menu.
+ */
 function showMobileMenu(){
 	document.getElementById("mobile-nav").style.display ="block";
 }
 
+
+/**
+ * Deploys the Submenu.
+ * @param {Event} event 
+ */
 function deploySubmenu(event){
 	var originID = event.target.attributes["id"].value;
 	
